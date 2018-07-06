a5bfaf22063a8f6be0caa104f90b24acba498783
y159632957@qq.com <y159632957@qq.com>
2018-07-06 14:47:10 +0800

    同上

--- sosopd/sosopd-order/src/main/java/cn/sosopd/task/dto/TaskDto.java
+++ sosopd/sosopd-order/src/main/java/cn/sosopd/task/dto/TaskDto.java
@@ -13,45 +13,45 @@ import lombok.experimental.Accessors;
 @Builder
 @Accessors(chain = true)
 public class TaskDto {
-	/**
-	 * 任务id
-	 */
-	@NotNull(groups = TaskUpdateValidatorGroup.class)
-	private Integer taskId;
+    /**
+     * 任务id
+     */
+    @NotNull(groups = TaskUpdateValidatorGroup.class)
+    private Integer taskId;
 
-	/**
-	 * 任务的工单
-	 */
-	private Integer orderId;
+    /**
+     * 任务的工单
+     */
+    private Integer orderId;
 
-	/**
-	 * 任务类型（在任务模板中定义）
-	 */
-	private String taskType;
+    /**
+     * 任务类型（在任务模板中定义）
+     */
+    private String taskType;
 
-	/**
-	 * 任务执行时间线
-	 */
-	private Date taskExecDatetime;
+    /**
+     * 任务执行时间线
+     */
+    private Date taskExecDatetime;
 
-	/**
-	 * 任务的状态
-	 */
-	private String taskStatus;
+    /**
+     * 任务的状态
+     */
+    private String taskStatus;
 
-	/**
-	 * 执行结果备注
-	 */
-	private String taskResult;
+    /**
+     * 执行结果备注
+     */
+    private String taskResult;
 
-	/**
-	 * 
-	 */
-	private Date createDatetime;
+    /**
+     * 
+     */
+    private Date createDatetime;
 
-	/**
-	 * 
-	 */
-	private Date lastSyncDatetime;
+    /**
+     * 
+     */
+    private Date lastSyncDatetime;
 
 }
